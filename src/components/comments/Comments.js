bef71110606391dac5224a7dc66840c337be96fa
Yoav Hirshberg <yoav.yh@gmail.com>
2021-07-24 20:29:05 +0300

    first commit

---
+++ src/components/comments/Comments.js
@@ -0,0 +1,69 @@
+import { useState, useEffect, useCallback } from 'react';
+import { useParams } from 'react-router-dom';
+
+import NewCommentForm from './NewCommentForm';
+import LoadingSpinner from '../UI/LoadingSpinner';
+import CommentsList from './CommentsList';
+import useHttp from '../../hooks/use-http';
+import { getAllComments } from '../../lib/api';
+import classes from './Comments.module.css';
+
+const Comments = () => {
+  const [isAddingComment, setIsAddingComment] = useState(false);
+  const params = useParams();
+  const { quoteId } = params;
+
+  const { sendRequest, status, data: loadedComments, error } = useHttp(getAllComments);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  },
+    [sendRequest, quoteId]);
+
+  const startAddCommentHandler = () => {
+    setIsAddingComment(true);
+  };
+
+  const addedCommentHandler = useCallback(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  let commentsToDisplay;
+
+  if (status === 'pending') {
+    commentsToDisplay = <div className='centered'>
+      <LoadingSpinner />
+    </div>
+  };
+
+  if (status === 'completed' && (loadedComments && loadedComments.length > 0)) {
+    commentsToDisplay = <CommentsList comments={loadedComments} />
+  };
+
+  if (status === 'completed' && (!loadedComments || loadedComments.length === 0)) {
+    commentsToDisplay = <p className='centered'>No comments were added yet</p>
+  };
+
+  if (status === 'error') {
+    commentsToDisplay = <div className='centered'>
+      {error}
+    </div>
+  };
+
+  return (
+    <section className={classes.comments}>
+      <h2>User Comments</h2>
+      {!isAddingComment && (
+        <button className='btn' onClick={startAddCommentHandler}>
+          Add a Comment
+        </button>
+      )}
+      {isAddingComment && <NewCommentForm
+        quoteId={quoteId}
+        onAddComment={addedCommentHandler} />}
+      {commentsToDisplay}
+    </section>
+  );
+};
+
+export default Comments;
---
+++ src/components/quotes/QuoteForm.js
@@ -0,0 +1,69 @@
+import { useRef, useState, Fragment } from 'react';
+import { Prompt } from 'react-router-dom';
+
+import Card from '../UI/Card';
+import LoadingSpinner from '../UI/LoadingSpinner';
+import classes from './QuoteForm.module.css';
+
+const QuoteForm = (props) => {
+  const authorInputRef = useRef();
+  const textInputRef = useRef();
+  const [isEntering, setIsEntering] = useState(false);
+  const [hasError, setHasError] = useState(false);
+
+  const validateInput = (input) => input.trim() !== '';
+
+  const submitFormHandler = (event) => {
+    event.preventDefault();
+    const enteredAuthor = authorInputRef.current.value;
+    const enteredText = textInputRef.current.value;
+    const isAuthorValid = validateInput(enteredAuthor);
+    const isTextValid = validateInput(enteredText);
+
+    if (isAuthorValid && isTextValid) {
+      props.onAddQuote({ author: enteredAuthor, text: enteredText });
+    } else {
+      setHasError(true);
+    };
+  };
+
+  const formFocusHandler = () => {
+    setIsEntering(true);
+  };
+
+  const finishEnteringHandler = () => {
+    setIsEntering(false);
+  };
+
+  return (
+    <Fragment>
+      <Prompt when={isEntering} message={() =>
+        'Are you sure you want to leave this page? all your work will be lost.'} />
+      <Card>
+        <form onFocus={formFocusHandler}
+          className={classes.form} onSubmit={submitFormHandler}>
+          {props.isLoading && (
+            <div className={classes.loading}>
+              <LoadingSpinner />
+            </div>
+          )}
+
+          <div className={classes.control}>
+            <label htmlFor='author'>Author</label>
+            <input type='text' id='author' ref={authorInputRef} />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor='text'>Text</label>
+            <textarea id='text' rows='5' ref={textInputRef} ></textarea>
+          </div>
+          {hasError && <p>Please fill both fields</p>}
+          <div className={classes.actions}>
+            <button onClick={finishEnteringHandler} className='btn'>Add Quote</button>
+          </div>
+        </form>
+      </Card>
+    </Fragment>
+  );
+};
+
+export default QuoteForm;
